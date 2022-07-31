a818e0ed040fe7d7b65e7868c2a95b2a77203b70
MatheusScarpim <matheus.cuan@nexum.com.br>
2022-07-31 11:09:59 -0300

    Alterações

--- DAL/Usuario_DAL.cs
+++ DAL/Usuario_DAL.cs
@@ -16,7 +16,7 @@ namespace DAL
             StringBuilder sb = new StringBuilder();
 
             sb.Append("INSERT INTO perfume ")
-              .Append("VALUES (DEFAULT, @nomeper, @qntd, @valor, @descricao);");
+              .Append("VALUES (DEFAULT, @nomeper, @qntd, @valor, @descricao, @excluido);");
             try
             {
                 using (NpgsqlConnection conn = new NpgsqlConnection(Funcoes.ConexaoBD.RetornaConexaoBD()))
@@ -26,6 +26,90 @@ namespace DAL
                     cmd.Parameters.AddWithValue("@qntd", usuario.Quantidade);
                     cmd.Parameters.AddWithValue("@valor", usuario.Valor);
                     cmd.Parameters.AddWithValue("@descricao", usuario.Descricao);
+                    cmd.Parameters.AddWithValue("@excluido", "n");
+
+                    conn.Open();
+
+                    return Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+            catch (NpgsqlException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public int Excluir(Modelo.Modelo_Usuario usuario)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("update perfume set excluido = 's' WHERE idperfume = @idperfume");
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(Funcoes.ConexaoBD.RetornaConexaoBD()))
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(sb.ToString(), conn);
+                    cmd.Parameters.AddWithValue("@idperfume", usuario.idPerfume);
+
+                    conn.Open();
+
+                    return Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+            catch (NpgsqlException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public int Recuperar(Modelo.Modelo_Usuario usuario)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("update perfume set excluido = 'n' WHERE idperfume = @idperfume");
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(Funcoes.ConexaoBD.RetornaConexaoBD()))
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(sb.ToString(), conn);
+                    cmd.Parameters.AddWithValue("@idperfume", usuario.idPerfume);
+
+                    conn.Open();
+
+                    return Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+            catch (NpgsqlException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public int Atualizar(Modelo.Modelo_Usuario usuario)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("UPDATE perfume SET nomeper = @nomeper, qntd = @qntd, valor = @valor, descricao = @descricao WHERE idperfume = @idperfume; ");
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(Funcoes.ConexaoBD.RetornaConexaoBD()))
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(sb.ToString(), conn);
+                    cmd.Parameters.AddWithValue("@idperfume", usuario.idPerfume);
+                    cmd.Parameters.AddWithValue("@nomeper", usuario.Nomeper);
+                    cmd.Parameters.AddWithValue("@qntd", usuario.Quantidade);
+                    cmd.Parameters.AddWithValue("@valor", usuario.Valor);
+                    cmd.Parameters.AddWithValue("@descricao", usuario.Descricao);
+                    cmd.Parameters.AddWithValue("@excluido", usuario.Excluido);
 
                     conn.Open();
 
--- TesteSQL/frmCadUsuario.cs
+++ TesteSQL/frmCadUsuario.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TesteSQL;
+using Npgsql;
 
 namespace AulaSqlServer
 {
     public partial class frmCadUsuario : Form
     {
+        private int rowIndex = -1;
         public frmCadUsuario()
         {
             InitializeComponent();
@@ -27,6 +29,7 @@ namespace AulaSqlServer
 
         private void bGravar_Click(object sender, EventArgs e)
         {
+            rowIndex = -1;
             int quantidade = Convert.ToInt32(txtQtd.Text);
             double valor = Convert.ToDouble(txtValor.Text);
             try
@@ -39,12 +42,149 @@ namespace AulaSqlServer
 
                 string codigo = new BLL.Usuario_BLL().Incluir(usuario).ToString();
 
-                MessageBox.Show("Usuario Cadstrado Com Sucesso o Id Gerrado e " + codigo);
+                MessageBox.Show("Usuario Cadstrado Com Sucesso o Id Gerado e " + codigo);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void MostrarTodos()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(Funcoes.ConexaoBD.RetornaConexaoBD());
+            NpgsqlCommand cmd;
+            DataTable dt;
+            string sql;
+            try
+            {
+                conn.Open();
+                sql = @"select * from perfume where excluido = 'n' order by idperfume;";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                conn.Close();
+                dgvDataSouce.DataSource = null;
+                dgvDataSouce.DataSource = dt;
+            }
+            catch(Exception ex)
+            {
+                conn.Close();
+            }
+        }
+        private void MostrarExcluidos()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(Funcoes.ConexaoBD.RetornaConexaoBD());
+            NpgsqlCommand cmd;
+            DataTable dt;
+            string sql;
+            try
+            {
+                conn.Open();
+                sql = @"select * from perfume where excluido = 's' order by idperfume;";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                conn.Close();
+                dgvDataSouce.DataSource = null;
+                dgvDataSouce.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+            }
+        }
+
+        private void frmCadUsuario_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            MostrarTodos();
+        }
+
+        private void dgvDataSouce_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                txtIdPerfume.Text = dgvDataSouce.Rows[e.RowIndex].Cells["idperfume"].Value.ToString();
+                txtNome.Text = dgvDataSouce.Rows[e.RowIndex].Cells["nomeper"].Value.ToString();
+                txtQtd.Text = dgvDataSouce.Rows[e.RowIndex].Cells["qntd"].Value.ToString();
+                txtValor.Text = dgvDataSouce.Rows[e.RowIndex].Cells["valor"].Value.ToString();
+                txtDescricao.Text = dgvDataSouce.Rows[e.RowIndex].Cells["descricao"].Value.ToString();
+                txtExcluido.Text = dgvDataSouce.Rows[e.RowIndex].Cells["excluido"].Value.ToString();
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            int idperfume = Convert.ToInt32(txtIdPerfume.Text);
+            try
+            {
+                Modelo.Modelo_Usuario usuario = new Modelo.Modelo_Usuario();
+                usuario.idPerfume = (idperfume);
+
+                string codigo = new BLL.Usuario_BLL().Excluir(usuario).ToString();
+
+                MessageBox.Show("O id " + idperfume + " foi excluido");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            MostrarExcluidos();
+        }
+
+        private void btnExcluidos_Click(object sender, EventArgs e)
+        {
+            MostrarExcluidos();
+        }
+
+        private void btnRecuperar_Click(object sender, EventArgs e)
+        {
+            int idperfume = Convert.ToInt32(txtIdPerfume.Text);
+            try
+            {
+                Modelo.Modelo_Usuario usuario = new Modelo.Modelo_Usuario();
+                usuario.idPerfume = (idperfume);
+
+                string codigo = new BLL.Usuario_BLL().Recuperar(usuario).ToString();
+
+                MessageBox.Show("O id " + idperfume + " foi recuperado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            MostrarTodos();
+        }
+
+        private void btnAttdados_Click(object sender, EventArgs e)
+        {
+            int quantidade = Convert.ToInt32(txtQtd.Text);
+            double valor = Convert.ToDouble(txtValor.Text);
+            int idperfume = Convert.ToInt32(txtIdPerfume.Text);
+            try
+            {
+                Modelo.Modelo_Usuario usuario = new Modelo.Modelo_Usuario();
+                usuario.idPerfume = (idperfume);
+                usuario.Nomeper = (txtNome.Text);
+                usuario.Quantidade = (quantidade);
+                usuario.Valor = (valor);
+                usuario.Descricao = (txtDescricao.Text);
+                usuario.Excluido = (txtExcluido.Text);
+
+                string codigo = new BLL.Usuario_BLL().Atualizar(usuario).ToString();
+
+                MessageBox.Show("Usuario Cadstrado Com Sucesso o Id Gerado e " + codigo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            MostrarTodos();
         }
     }
 }
