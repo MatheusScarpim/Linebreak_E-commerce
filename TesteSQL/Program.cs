e3d0e75fe24410431fa7652bb5667f6bda24a42f
Pedro-Kayami <pedro.kayami@nexum.com.br>
2022-11-07 20:28:34 -0300

    salvo

--- TesteSQL/Program.cs
+++ TesteSQL/Program.cs
@@ -16,7 +16,7 @@ namespace TesteSQL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCadProduto());
+            Application.Run(new frmMenu());
         }
     }
 }
